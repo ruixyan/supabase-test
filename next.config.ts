5cde663dc5df92475dd95e11ff18b4a3186886c5
ruixyan <168030092+ruixyan@users.noreply.github.com>
2026-06-12 14:20:11 -0400

    file update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kanmbejtvkwifhibskow.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
